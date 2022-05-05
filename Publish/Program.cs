e31e8a7f1d607a7862a39e99102794f689dbb291
Omar BENARCHID <omar.benarchid@gxo.com>
2022-05-05 17:21:55 +0000

    First commit

---
+++ Publish/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+using NATS.Client;
+using System.Text;
+
+
+
+// Define as variables
+var subject = "order";
+var message = "{ amount: 100.0 }";
+
+//// Get the connection
+var connection = GetConnection();
+
+//// Publish the message
+connection.Publish(subject, Encoding.UTF8.GetBytes(message));
+
+Console.WriteLine($"{DateTime.Now:F} - Subscription: {subject} -  Send: {message}");
+
+
+
+/// <summary>
+/// Get a new connection
+/// </summary>
+/// <returns>New connection with NATs.io</returns>
+static IConnection GetConnection()
+{
+    var cf = new ConnectionFactory();
+    var c = cf.CreateConnection();
+
+    return c;
+}
---
+++ Subscribe/Program.cs
@@ -0,0 +1,34 @@
+﻿using NATS.Client;
+using NATS.Client.Rx;
+using NATS.Client.Rx.Ops;
+using System.Text;
+
+// Get the connection
+var connection = GetConnection();
+
+// Signature
+var sAsyncAll = connection.Observe("order").Select(s => s.Data);
+
+sAsyncAll.Subscribe(message => Console.WriteLine($"{DateTime.Now:F} - All: "
+    + Encoding.UTF8.GetString(message)));
+
+// Wait for a key to be pressed
+Console.ReadLine();
+
+// Disconnecting
+connection.Drain();
+connection.Close();
+
+
+
+/// <summary>
+/// Get a new connection
+/// </summary>
+/// <returns>New connection with NATs.io</returns>
+static IConnection GetConnection()
+{
+    var cf = new ConnectionFactory();
+    var c = cf.CreateConnection();
+
+    return c;
+}
